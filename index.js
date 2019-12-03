cc3b41972e1aada92e02359d9f86c61292867c83
Pierre-Gilles Levallois <pierregille.levallois@gmail.com>
2019-12-03 10:46:25 +0100

    Add socket Layer for ATMO and RTE Data

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const port = 8000;
 const express = require('express')
 const app = express()
 const conf = require('./config/config.json');
-var index = require('./routes/index');
+// var index = require('./routes/index');
+
+var http = require('http').Server(app); //
+var io = require('socket.io')(http);
 
 // view engine setup and static routes
 app.set('view engine', 'ejs');
@@ -13,6 +16,7 @@ app.use('/data', express.static(__dirname + '/data')); // Routing data
 app.use('/media', express.static(__dirname + '/media'));
 app.use('/js/jquery', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
 app.use('/js/slick', express.static(__dirname + '/node_modules/slick-carousel/slick')); // redirect to slick
+app.use('/js/socket.io', express.static(__dirname + '/node_modules/socket.io')); // redirect to socket.io
 app.use('/css', express.static(__dirname + '/public/css')); // Routing css
 app.use('/font', express.static(__dirname + '/public/font')); // Routing font
 
@@ -33,6 +37,23 @@ app.post('/', function (req, res) {
   // res.send('POST request to the homepage');
 });
 
-app.listen(port, function () {
-  console.log('Example app listening on port '+port+'!')
-})
+http.listen(port, function(){
+  console.log('Http server is listening on port %d', port);
+});
+
+
+/*
+  Services de socket pour les mises à jours de données RTE et ATMO
+*/
+io.on('connection', function (socket) {
+
+  socket.data = {};
+  
+  socket.on('login_client', function(){
+    console.log('Browser is ready !')
+  });
+
+	socket.on('get_atmo_data', function() {
+
+	});
+});
