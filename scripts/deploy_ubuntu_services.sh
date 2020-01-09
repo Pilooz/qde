f9c823563b6b5d9c4d738d0b8f0275560537d5fc
Pierre-Gilles Levallois <pierregilles.levallois@gmail.com>
2020-01-09 11:23:26 +0100

    ajout d'un script de déploiement des services systemd

---
+++ scripts/deploy_ubuntu_services.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+cd /etc/systemd/system
+
+sudo systemctl disable qde_datacnrd.service
+sudo systemctl disable qde_deployd.service
+sudo systemctl disable qde_ngrokd.service
+sudo systemctl disable qde_node_server.service
+sudo systemctl disable qde_presenced.service
+
+sudo ln -s /home/cnr/Documents/quai-des-energies/services/ubuntu/qde_datacnrd.service qde_datacnrd.service
+sudo systemctl enable qde_datacnrd.service
+sudo ln -s /home/cnr/Documents/quai-des-energies/services/ubuntu/qde_deployd.service qde_deployd.service
+sudo systemctl enable qde_deployd.service
+sudo ln -s /home/cnr/Documents/quai-des-energies/services/ubuntu/qde_ngrokd.service qde_ngrokd.service
+sudo systemctl enable qde_ngrokd.service
+sudo ln -s /home/cnr/Documents/quai-des-energies/services/ubuntu/qde_node_server.service qde_node_server.service
+sudo systemctl enable qde_node_server.service
+sudo ln -s /home/cnr/Documents/quai-des-energies/services/ubuntu/qde_presenced.service qde_presenced.service
+sudo systemctl enable qde_presenced.service
+
+sudo systemctl daemon-reload
