bdc57c6d3b4fc6cbb5527e034c59bb540db0c922
Pierre-Gilles Levallois <pierregilles.levallois@gmail.com>
2020-01-10 10:11:43 +0100

    cron pour les data de la cnr

--- scripts/deploy_ubuntu_services.sh
+++ scripts/deploy_ubuntu_services.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 cd /etc/systemd/system
 
-sudo systemctl disable qde_datacnrd.service
+#sudo systemctl disable qde_datacnrd.service
 sudo systemctl disable qde_deployd.service
 sudo systemctl disable qde_ngrokd.service
 sudo systemctl disable qde_node_server.service
 sudo systemctl disable qde_presenced.service
 
-sudo ln -s /home/cnr/Documents/quai-des-energies/services/ubuntu/qde_datacnrd.service qde_datacnrd.service
-sudo systemctl enable qde_datacnrd.service
+#sudo ln -s /home/cnr/Documents/quai-des-energies/services/ubuntu/qde_datacnrd.service qde_datacnrd.service
+#sudo systemctl enable qde_datacnrd.service
 sudo ln -s /home/cnr/Documents/quai-des-energies/services/ubuntu/qde_deployd.service qde_deployd.service
 sudo systemctl enable qde_deployd.service
 sudo ln -s /home/cnr/Documents/quai-des-energies/services/ubuntu/qde_ngrokd.service qde_ngrokd.service
